4707dcd46d97a54ec8228b9c4734be603c9a8592
Angelo <Angeloponce25@gmail.com>
2026-02-24 22:16:00 -0500

    240226

--- demo.php
+++ demo.php
@@ -10,7 +10,7 @@ $conexion = new mysqli($servidor, $usuario, $contrasena, $base_de_datos);
 
 // Verificar la conexión
 if ($conexion->connect_error) {
-    die("La conexión ha fallado: " . $coni->connect_error);
+    die("La conexión ha fallado : " . $coni->connect_error);
 }
 
 // Establecer el conjunto de caracteres
