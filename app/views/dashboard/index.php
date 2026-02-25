ee317659d406a840a221757e98a15264a2454b3b
Angelo <Angeloponce25@gmail.com>
2026-02-24 23:09:35 -0500

    240226

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -32,7 +32,7 @@ if (!empty($paciente['pac_fecnasc'])) {
                  src="<?= BASE_URL ?>/img/user.png">
 
             <h3 class="profile-username text-center">
-              <?= $data['pac_nombres'] ?> <?= $data['pac_primer_ape'] ?> <?= $data['pac_segundo_ape'] ?>
+              <?= $data[0] ['pac_nombres'] ?> <?= $data[0] ['pac_primer_ape'] ?> <?= $data[0] ['pac_segundo_ape'] ?>
             </h3>
 
             <p class="text-muted text-center">
