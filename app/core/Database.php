53dbeb103fb4fab91d33872fc4149c3ca2fc78d6
Angelo <Angeloponce25@gmail.com>
2026-02-24 21:51:55 -0500

    240226

--- app/core/Database.php
+++ app/core/Database.php
@@ -2,11 +2,37 @@
 
 class Database {
 
-  
+    private $conn;
 
     public function __construct() {
 
-        echo gethostbyname("192.168.1.151");
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+        $this->conn = mysqli_init();
+
+        // ⏱ aumentar tiempo de espera
+        $this->conn->options(MYSQLI_OPT_CONNECT_TIMEOUT, 10);
+
+        $this->conn->real_connect(
+            "192.168.1.151",
+            "webmaster",
+            "super",
+            "gperu_cmdn",
+            3306
+        );
+
+        $this->conn->set_charset("utf8");
     }
 
+    public function query($sql) {
+        return $this->conn->query($sql);
+    }
+
+    public function execute($sql) {
+        return $this->conn->query($sql);
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
 }
