f7914d958fddbc93ca82ae3eb2b333526ea5e4cc
Angelo <Angeloponce25@gmail.com>
2026-02-24 23:02:33 -0500

    240226

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../app/models/User.php';
+/*require_once '../app/models/User.php';*/
 
 class DashboardController extends Controller {
 
@@ -15,6 +15,8 @@ class DashboardController extends Controller {
 
     public function index()
     {
+
+        require_once APP_PATH . '/models/User.php';
         $userModel = new User();
 
         $paciente = $userModel->getPacienteById($_SESSION['user']['id_paciente']);
