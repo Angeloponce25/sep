9e6a91ac19cf1ed1c043c5667dfecd85f3e0ff99
Angelo <Angeloponce25@gmail.com>
2026-03-08 09:36:43 -0500

    080326

--- app/models/Evaluaciones.php
+++ app/models/Evaluaciones.php
@@ -12,10 +12,20 @@ class Evaluaciones {
 
     public function getEvaluacionesPaciente($id)
     {
-        $sql = "SELECT * FROM piv_testpsicologico WHERE id_paciente = $id";
+        $sql = "SELECT 
+                    p.id_evaluacion,
+                    p.estado_examen,
+                    t.titulo,
+                    t.descripcion,
+                    t.icono
+                FROM piv_testpsicologico p
+                INNER JOIN testpsicologico t 
+                    ON t.id_test = p.id_test
+                WHERE p.id_paciente = $id
+                AND t.estado = 1";
 
         $result = $this->db->query($sql);
 
-        return $result->fetch_assoc();
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
 }
