333b88c6febe101f9c26257536951604e3889c5e
Angelo <Angeloponce25@gmail.com>
2026-03-08 12:54:05 -0500

    080326

--- app/views/evaluaciones/examen.php
+++ app/views/evaluaciones/examen.php
@@ -22,22 +22,11 @@ $id_evaluacion = $data['id_evaluacion'];
 
 <div class="box-body">
 
-<div class="progress">
-<div id="barraProgreso" class="progress-bar progress-bar-success" style="width:0%">
-</div>
-</div>
+<h4>ID Evaluación: <?php echo $id_evaluacion; ?></h4>
 
-<br>
-
-<h4 id="preguntaTexto"></h4>
-
-<div id="opciones"></div>
-
-<br>
-
-<button id="btnSiguiente" class="btn btn-primary pull-right">
-Siguiente
-</button>
+<pre>
+<?php print_r($preguntas); ?>
+</pre>
 
 </div>
 
@@ -49,83 +38,3 @@ Siguiente
 </section>
 
 </div>
-
-<script>
-
-const examen = <?= json_encode($preguntas, JSON_UNESCAPED_UNICODE); ?>;
-const idEvaluacion = <?= $id_evaluacion ?>;
-
-let preguntaActual = 0;
-let respuestas = [];
-
-function cargarPregunta() {
-
-    let pregunta = examen[preguntaActual];
-
-    document.getElementById("preguntaTexto").innerHTML =
-        (preguntaActual + 1) + ". " + pregunta.pregunta;
-
-    let html = "";
-
-    pregunta.opciones.forEach((op, i) => {
-
-        html += `
-        <div class="radio">
-            <label>
-                <input type="radio" name="respuesta" value="${i}">
-                ${op}
-            </label>
-        </div>
-        `;
-
-    });
-
-    document.getElementById("opciones").innerHTML = html;
-
-    actualizarBarra();
-
-}
-
-function actualizarBarra(){
-
-    let progreso = ((preguntaActual) / examen.length) * 100;
-
-    document.getElementById("barraProgreso").style.width = progreso + "%";
-
-}
-
-document.getElementById("btnSiguiente").addEventListener("click", function(){
-
-    let seleccion = document.querySelector('input[name="respuesta"]:checked');
-
-    if(!seleccion){
-
-        alert("Selecciona una opción");
-
-        return;
-
-    }
-
-    respuestas[preguntaActual] = seleccion.value;
-
-    preguntaActual++;
-
-    if(preguntaActual >= examen.length){
-
-        document.getElementById("barraProgreso").style.width = "100%";
-
-        console.log("Respuestas:", respuestas);
-
-        alert("Examen terminado");
-
-        return;
-
-    }
-
-    cargarPregunta();
-
-});
-
-cargarPregunta();
-
-</script>
