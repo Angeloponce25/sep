618e33c12548f2a80f51b82c211c8f6c148895dd
Angelo <Angeloponce25@gmail.com>
2026-03-08 11:38:29 -0500

    080326

--- app/views/evaluaciones/examen.php
+++ app/views/evaluaciones/examen.php
@@ -42,3 +42,83 @@ Siguiente
 </section>
 
 </div>
+
+<script>
+
+const examen = <?= $preguntas ?>;
+
+let preguntaActual = 0;
+let respuestas = [];
+
+function cargarPregunta() {
+
+  document.getElementById("preguntaTexto").innerHTML =
+    examen[preguntaActual].pregunta;
+
+  let html = "";
+
+  examen[preguntaActual].opciones.forEach((op, i) => {
+
+    html += `
+      <div class="radio">
+        <label>
+          <input type="radio" name="respuesta" value="${i}">
+          ${op}
+        </label>
+      </div>
+    `;
+
+  });
+
+  document.getElementById("opciones").innerHTML = html;
+
+  actualizarBarra();
+}
+
+function actualizarBarra() {
+
+let progreso = ((preguntaActual+1)/examen.length)*100;
+
+document.getElementById("barraProgreso").style.width = progreso+"%";
+
+}
+
+document.getElementById("btnSiguiente").addEventListener("click",()=>{
+
+let seleccion=document.querySelector('input[name="respuesta"]:checked');
+
+if(!seleccion){
+alert("Selecciona una opción");
+return;
+}
+
+respuestas.push(seleccion.value);
+
+preguntaActual++;
+
+if(preguntaActual < examen.length){
+
+cargarPregunta();
+
+}else{
+
+finalizarExamen();
+
+}
+
+});
+
+function finalizarExamen(){
+
+document.querySelector(".box-body").innerHTML=`
+<div class="alert alert-success text-center">
+<h3><i class="fa fa-check"></i> Examen finalizado</h3>
+<p>Gracias por completar la evaluación</p>
+</div>
+`;
+
+}
+
+cargarPregunta();
+
+</script>
