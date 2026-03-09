23ce20c390f9b88935056349a9300a8e2851b746
Angelo <Angeloponce25@gmail.com>
2026-03-08 23:31:34 -0500

    080326

--- app/controllers/EvaluacionesController.php
+++ app/controllers/EvaluacionesController.php
@@ -52,7 +52,40 @@ class EvaluacionesController extends Controller {
     public function guardarRespuestas()
     {
 
-        echo 'hola';
+        /*if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+            $id_evaluacion = $_POST['id_evaluacion'];
+            $respuestas = $_POST['respuestas'];
+
+            // obtener datos del pivote
+            $info = $this->evaluacionesModel->getEvaluacionInfo($id_evaluacion);
+
+            if(!$info){
+                echo "error";
+                return;
+            }
+
+            $id_paciente = $info['id_paciente'];
+            $id_acto = $info['id_acto_medico'];
+
+            foreach($respuestas as $id_pregunta => $id_opcion){
+
+                $this->evaluacionesModel->guardarRespuesta(
+                    $id_evaluacion,
+                    $id_pregunta,
+                    $id_opcion,
+                    $id_paciente,
+                    $id_acto
+                );
+
+            }
+
+            // marcar examen como terminado
+            $this->evaluacionesModel->finalizarExamen($id_evaluacion);
+
+            echo "success";
+
+        }*/
 
     }
 
